73abc24af8008bf9e30d5b7c73d197a476fed7c7
kelsonthony <k.thony@gmail.com>
2023-05-26 09:20:19 -0300

    Update project

---
+++ mensagem/src/main/java/com/algaworks/example/mensagem/security/JPAUsuarioDetailsService.java
@@ -0,0 +1,27 @@
+package com.algaworks.example.mensagem.security;
+
+import com.algaworks.example.mensagem.domain.UsuarioRepository;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+public class JPAUsuarioDetailsService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public JPAUsuarioDetailsService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return usuarioRepository.findByEmail(email)
+                .orElseThrow(()-> new UsernameNotFoundException(email));
+    }
+
+}
---
+++ mensagem/src/main/java/com/algaworks/example/mensagem/security/SegurancaBasicConfig.java
@@ -0,0 +1,52 @@
+package com.algaworks.example.mensagem.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SegurancaBasicConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JPAUsuarioDetailsService userDetailsService;
+    
+    @Autowired
+    private InMemoryUserDetailsManager inMemoryUserDetailsManager;
+    
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder);
+        authenticationManagerBuilder
+        		.userDetailsService(inMemoryUserDetailsManager)
+        		.passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET,"/mensagens/**","/paises").permitAll()
+                .antMatchers(HttpMethod.POST, "/mensagens").hasAnyRole("CLIENTE","ADMIN")
+                .antMatchers(HttpMethod.GET, "/gerenciador/saude", "/gerenciador/info").permitAll()
+                .antMatchers("/gerenciador/**").hasRole("ACTUATOR")
+            .and()
+                .csrf().disable()
+                .httpBasic();
+    }
+
+}
